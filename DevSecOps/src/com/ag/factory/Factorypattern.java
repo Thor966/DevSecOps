d42391babf39624f4ae9930e2fbfe3db29f66bbd
Aniket Gawande <aniketgawande966@gmail.com>
2025-01-08 18:57:04 +0530

    changes by the TL in factory package and main pacakge

--- DevSecOps/src/com/ag/factory/Factorypattern.java
+++ DevSecOps/src/com/ag/factory/Factorypattern.java
@@ -1,5 +1,29 @@
 package com.ag.factory;
 
-public class Factorypattern {
+import com.ag.comp.Car;
+import com.ag.comp.ElectricCar;
+import com.ag.comp.LuxoryCar;
+import com.ag.comp.OffroadCar;
+import com.ag.comp.StandardCar;
+
+public class Factorypattern 
+{
+	public static Car orderCar(String type)
+	{
+		Car car = null;
+		
+		if(type.equalsIgnoreCase("standard"))
+			car = new StandardCar();
+		else if(type.equalsIgnoreCase("offroad"))
+			car = new OffroadCar();
+		else if(type.equalsIgnoreCase("luxory"))
+			car = new LuxoryCar();
+		else if(type.equalsIgnoreCase("electric"))
+			car = new ElectricCar();
+		else
+			throw new IllegalArgumentException("Invalid car type...");
+		
+		return car;
+	}
 
 }
--- DevSecOps/src/com/ag/main/FactorypatternTest.java
+++ DevSecOps/src/com/ag/main/FactorypatternTest.java
@@ -1,9 +1,29 @@
 package com.ag.main;
 
+import com.ag.comp.Car;
+import com.ag.factory.Factorypattern;
+
 public class FactorypatternTest {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)
+	{
+		Car car1 = Factorypattern.orderCar("standard");
+		car1.car();
+		System.out.println("================================");
+		
+		Car car2 = Factorypattern.orderCar("offroad");
+		car2.car();
+		System.out.println("================================");
+		
+		Car car3 = Factorypattern.orderCar("luxory");
+		car3.car();
+		System.out.println("================================");
+		
+		Car car4 = Factorypattern.orderCar("electric");
+		car4.car();
+		
+		
+		
 
 	}
 
